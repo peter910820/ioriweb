a470666fa19ee6b61904b64b64aaf302d2d0f31a
peter910820 <peter910820@gmail.com>
2023-04-17 23:41:35 +0800

    fix test4

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ async def submmit(request: Request, information : list = Form(...)):
     return templates.TemplateResponse('/submmit.html',{'request':request})
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 5000))
     uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
