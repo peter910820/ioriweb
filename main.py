61d97fcb26c3fd46faabeb9f80a6c0ca0ff3c6e6
peter910820 <peter910820@gmail.com>
2023-05-01 02:56:42 +0800

    Replanning route and add hanamaruWeb route(personal web)

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ import os, psycopg2
 
 from src.data_control import DataControl
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static/css", StaticFiles(directory="static/css"), name="static/css")
 templates = Jinja2Templates(directory="templates")
+galgame = Jinja2Templates(directory="templates/GalgameWeb")
+hanamaru = Jinja2Templates(directory="templates/HanamaruWeb")
 
 # @app.get("/home/{home}")
 # async def index(home):
@@ -18,53 +21,56 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
+    return templates.TemplateResponse('index.html',{'request': request})
+#------------------------------------------------------------------------------#
+
+@app.get("/hanamaru", response_class=HTMLResponse)
+async def galgameRoot(request: Request):
+    return hanamaru.TemplateResponse('main.html',{'request': request})
+
+#------------------------------------------------------------------------------#
+
+@app.get("/galgame", response_class=HTMLResponse)
+async def galgameRoot(request: Request):
     db = psycopg2.connect('postgres://seaotter:OC5okdJZpXu3zo8RSmpKyyowcfrawdPh@dpg-cgpajv0u9tun42shmebg-a.oregon-postgres.render.com/ioriweb')
     cursor = db.cursor()
     cursor.execute('''SELECT * FROM galgameTitle;''')
     data = cursor.fetchall()
-    return templates.TemplateResponse('home.html',{'request': request,'data' : data})
+    return galgame.TemplateResponse('home.html',{'request': request,'data' : data})
 
-@app.get("/resource", response_class=HTMLResponse)
+@app.get("/galgame/resource", response_class=HTMLResponse)
 async def resource(request: Request):
-    return templates.TemplateResponse('resource.html',{'request':request})
+    return galgame.TemplateResponse('resource.html',{'request':request})
 
-@app.get("/databaseOperate", response_class=HTMLResponse)
+@app.get("/galgame/databaseOperate", response_class=HTMLResponse)
 async def databaseOperate(request: Request):
-    return templates.TemplateResponse('databaseoperate.html',{'request':request})
+    return galgame.TemplateResponse('databaseoperate.html',{'request':request})
 
-@app.get("/note", response_class=HTMLResponse)
-async def note(request: Request):
-    return templates.TemplateResponse('note.html',{'request':request})
-
-@app.get("/tag/{tagName}", response_class=HTMLResponse)
+@app.get("/galgame/tag/{tagName}", response_class=HTMLResponse)
 async def tagName(request: Request, tagName):
     data_control = DataControl()
     titleList = data_control.searchTag(tagName)
-    return templates.TemplateResponse('/tagname.html',{'request': request, 'tagName':tagName, 'titleList':titleList})
+    return galgame.TemplateResponse('/tagname.html',{'request': request, 'tagName':tagName, 'titleList':titleList})
 
-@app.get("/article/{articleTitle}", response_class=HTMLResponse)
+@app.get("/galgame/article/{articleTitle}", response_class=HTMLResponse)
 async def articleTitle(request: Request, articleTitle):
     data_control = DataControl()
     articleData = data_control.searchArticle(articleTitle)
-    return templates.TemplateResponse('/article.html',{'request': request, 'articleData' : articleData[0]})
+    return galgame.TemplateResponse('/article.html',{'request': request, 'articleData' : articleData[0]})
 
-@app.get("/aboutme", response_class=HTMLResponse)
+@app.get("/galgame/aboutme", response_class=HTMLResponse)
 async def aboutme(request: Request):
-    return templates.TemplateResponse('/aboutme.html',{'request':request})
+    return galgame.TemplateResponse('/aboutme.html',{'request':request})
 
-@app.get("/newGalgameArticle", response_class=HTMLResponse)
+@app.get("/galgame/newGalgameArticle", response_class=HTMLResponse)
 async def newGalgameArticle(request: Request):
     db = psycopg2.connect('postgres://seaotter:OC5okdJZpXu3zo8RSmpKyyowcfrawdPh@dpg-cgpajv0u9tun42shmebg-a.oregon-postgres.render.com/ioriweb')
     cursor = db.cursor()
     cursor.execute('''SELECT * FROM tag;''')
     data = cursor.fetchall()
-    return templates.TemplateResponse('/newGalgameArticle.html',{'request':request, 'data':data})
+    return galgame.TemplateResponse('/newGalgameArticle.html',{'request':request, 'data':data})
 
-@app.get("/newArticle", response_class=HTMLResponse)
-async def newArticle(request: Request):
-    return templates.TemplateResponse('/newArticle.html',{'request':request})
-
-@app.post("/submmit", response_class=HTMLResponse)
+@app.post("/galgame/submmit", response_class=HTMLResponse)
 async def submmit(request: Request, information : list = Form(...)):
     if information[-1] == '0000':
         information.pop(-1)
@@ -79,9 +85,9 @@ async def submmit(request: Request, information : list = Form(...)):
         data_control.galgameArticle_insert(article_array, articleTitle_array)
     else:
         print('password error')
-    return templates.TemplateResponse('/submmit.html',{'request':request})
+    return galgame.TemplateResponse('/submmit.html',{'request':request})
 
-@app.post("/sabi", response_class=HTMLResponse)
+@app.post("/galgame/sabi", response_class=HTMLResponse)
 async def sabi(request: Request, tagInformation : list = Form(...)):
     if tagInformation[-1] == '1111':
         tagInformation.pop(-1)
@@ -90,8 +96,8 @@ async def sabi(request: Request, tagInformation : list = Form(...)):
         data_control.tagName_insert(tagName)
     else:
         print('password error')
-    return templates.TemplateResponse('/submmit.html',{'request':request})
+    return galgame.TemplateResponse('/submmit.html',{'request':request})
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get('PORT', 5000))
-#     uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
---
+++ static/main.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $("input").focus(function(){
+//     $("input").css("background-color","cornflowerblue");
+//     });
+//     $("input").blur(function(){
+//     $("input").css("background-color","#white");
+//     });
+// });
+  
